5ce5d139e9df0c9fbd37ec223c9f659531037dbf
Lushen Moodley <lushenmoodley@yahoo.com>
2025-05-06 20:23:15 +0200

    added category ui

---
+++ ECommerceApp/ECommerceApp/Repository/CategoryRepository.cs
@@ -0,0 +1,69 @@
+﻿using ECommerceApp.Data;
+using ECommerceApp.Repository.IRespository;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace ECommerceApp.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        } 
+
+        public Category Create(Category obj)
+        {
+            _db.Category.Add(obj);
+            _db.SaveChanges();
+            return obj;
+        }
+
+        public bool Delete(int id)
+        {
+            var obj = _db.Category.FirstOrDefault(u => u.Id == id);
+            
+            if(obj!=null)
+            {
+                _db.Category.Remove(obj);
+                return _db.SaveChanges()>0;
+            }
+
+            return false;
+        }
+
+        public Category Get(int id)
+        {
+            var obj = _db.Category.FirstOrDefault(u => u.Id == id);
+
+            if(obj==null)
+            {
+                return new Category();
+            }
+
+            return obj;
+        }
+
+        public IEnumerable<Category> GetAll()
+        {
+            return _db.Category.ToList();
+        }
+
+        public Category Update(Category obj)
+        {
+            var objFromDB = _db.Category.FirstOrDefault(x => x.Id == obj.Id);
+
+            if(objFromDB is not null)
+            {
+                objFromDB.Name = obj.Name;
+                _db.Category.Update(objFromDB);
+                _db.SaveChanges();
+                return objFromDB;
+            }
+
+            return obj;
+        }
+    }
+}
+    
---
+++ ECommerceApp/ECommerceApp/Repository/IRespository/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using ECommerceApp.Data;
+
+namespace ECommerceApp.Repository.IRespository
+{
+    public interface ICategoryRepository
+    {
+        public Category Create(Category obj);
+        public Category Update(Category obj);
+        public bool Delete(int id);
+        public Category Get(int id);
+        public IEnumerable<Category> GetAll();
+    }
+}
