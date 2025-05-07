d3173c9f5d664615fdba8a3801ae0ef934e706f9
Lushen Moodley <lushenmoodley@yahoo.com>
2025-05-07 20:08:51 +0200

    changes

--- ECommerceApp/ECommerceApp/Program.cs
+++ ECommerceApp/ECommerceApp/Program.cs
@@ -4,6 +4,8 @@ using Microsoft.EntityFrameworkCore;
 using ECommerceApp.Components;
 using ECommerceApp.Components.Account;
 using ECommerceApp.Data;
+using ECommerceApp.Repository.IRespository;
+using ECommerceApp.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +14,7 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IdentityUserAccessor>();
 builder.Services.AddScoped<IdentityRedirectManager>();
 builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
